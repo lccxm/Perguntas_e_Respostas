0b900473cae1a776c069382709c8c1aa3863e76f
lccxm <luccamolon@gmail.com>
2020-10-01 11:55:34 -0300

    gerando tabelas mysql com modal

---
+++ database/Question.js
@@ -0,0 +1,15 @@
+const sequelize = require('sequelize');
+const connection = require('./database');
+
+const Question = connection.define('question',{
+    title:{
+        type: sequelize.STRING,
+        allowNull: false
+    },
+    description:{
+        type: sequelize.TEXT,
+        allowNull: false
+    }
+})
+
+Question.sync({force: false}).then(()=>{})
